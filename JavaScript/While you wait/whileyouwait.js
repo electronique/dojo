1f6ea2c2eb5e8aef18cdf339cd17779f6e990300
electronique <joe@joereinsel.org>
2017-08-08 13:40:56 -0400

    added While you wait script

--- JavaScript/While you wait/whileyouwait.js
+++ JavaScript/While you wait/whileyouwait.js
@@ -1,11 +1,36 @@
-x = 40;
 
-  while(x>30){
-x--;
-    console.log( x+ " days until my birthday, how sad :\-\(");
-}
-while(x<=30){
-  x--;
-    console.log(x+ "DAYS UNITL MY BIRTHDAY!!");
+/*For every day, print how many days left.
 
+If it's more than 30 days, print a sad message.
+
+If it's less than 30 days, print a message that sounds excited!
+
+If it's less than 5 days, SCREAM IT!
+
+ONCE IT'S YOUR BIRTHDAY HAVE PARTY!
+
+60 days until my birthday. Such a long time... :(
+59 days until my birthday. Such a long time... :(
+58 days until my birthday. Such a long time... :(
+2 DAYS UNTIL MY BIRTHDAY!!!
+1 DAY UNTIL MY BIRTHDAY!!!
+♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*
+♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪
+*•♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•«*/
+
+var daysUntilMyBirthday = 20;
+
+while(daysUntilMyBirthday>=0){
+
+      if(daysUntilMyBirthday>=15){
+          console.log( daysUntilMyBirthday+ " days until my birthday, how sad :\-\(");
+      }
+      if(daysUntilMyBirthday>0){
+              console.log(daysUntilMyBirthday+ " DAYS UNITL MY BIRTHDAY!!");
+
+          }
+      if(daysUntilMyBirthday===0){
+              console.log("HAPPY BIRTHDAY!!!!!, It my day!");
+            }
+daysUntilMyBirthday--;
 }
